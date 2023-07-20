00b69af94ce484e003c19adb226aa23f23197eff
Kacper <kacp.bury@gmail.com>
2023-07-20 21:11:27 +0200

    TT-3 - Added assets

---
+++ app/Livewire/VaultComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Asset;
+use Livewire\Component;
+
+class VaultComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.vault-component');
+    }
+}
